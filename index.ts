4deecb8a4587194975d076007ec8fb046751ff4c
Your Name <you@example.com>
2024-03-30 12:41:11 -0700

    Push my simple calculator to github

---
+++ index.ts
@@ -0,0 +1,35 @@
+#! /usr/bin/env node
+
+import inquirer from "inquirer";
+import chalk from "chalk";
+
+const answer = await inquirer.prompt(
+    [
+        {
+            name: "firstNumber",
+            type: "number",
+            message: (chalk.greenBright("Enter your first number"))
+        },
+        {
+            name: "secondNumber",
+            type: "number",
+            message: (chalk.greenBright("Enter your second number"))
+        },
+        {
+            name: "operator",
+            type: "list",
+            message: (chalk.yellow("Select one of the operator to perform operations")),
+            choices: ["Addition", "Subtraction","Multiplication","Division"]
+        }
+    ]
+)
+
+if (answer.operator === "Addition"){
+    console.log(answer.firstNumbernumber + answer.secondNumber)
+} else if (answer.operator === "Subtraction") {
+    console.log(answer.firstNumber - answer.secondNumber)
+} else if (answer.operator === "Multiplication") {
+    console.log(answer.firstNumber * answer.secondNumber)
+} else if (answer.operator === "Division") {
+    console.log(answer.firstNumber / answer.secondNumber)
+}
